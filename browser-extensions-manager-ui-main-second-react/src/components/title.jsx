dcd7fb132ea178027dd29b61da1294ef87392110
amira <amirazadehranjbar@gmail.com>
2025-09-05 10:57:03 -0700

    apply active style to buttons

--- browser-extensions-manager-ui-main-second-react/src/components/title.jsx
+++ browser-extensions-manager-ui-main-second-react/src/components/title.jsx
@@ -1,8 +1,9 @@
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {setFilterData} from "../redux/features/extentionReducer.js";
 
 const Title = () => {
 
+    const {filterData} = useSelector((state)=>state.extension);
 
     const dispatch = useDispatch()
     const handleFilterDataChange = (f)=>{
@@ -14,14 +15,14 @@ const Title = () => {
         <div className="title">
             <h2>Extention List</h2>
             <div>
-                <button className="title-button" onClick={()=>handleFilterDataChange("all")}>all</button>
+                <button className={filterData==="all" ? "title-button-active" : "title-button"} onClick={()=>handleFilterDataChange("all")}>all</button>
 
 
-                <button className="title-button" onClick={()=>{
+                <button className={filterData==="active" ? "title-button-active" : "title-button"} onClick={()=>{
                     handleFilterDataChange("active")}}>active</button>
 
 
-                <button className="title-button" onClick={()=>{
+                <button className={filterData==="inactive" ? "title-button-active" : "title-button"} onClick={()=>{
                     handleFilterDataChange("inactive")}}>inactive</button>
             </div>
         </div>
