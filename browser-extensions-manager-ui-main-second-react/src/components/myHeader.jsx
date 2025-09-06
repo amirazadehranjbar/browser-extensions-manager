4ebfabb5e918665c4df3899ddfc8e4e309f80399
amira <amirazadehranjbar@gmail.com>
2025-09-06 09:47:43 -0700

    apply switch theme toggle

--- browser-extensions-manager-ui-main-second-react/src/components/myHeader.jsx
+++ browser-extensions-manager-ui-main-second-react/src/components/myHeader.jsx
@@ -1,31 +1,39 @@
-import {Blocks, Moon, SunIcon} from "lucide-react";
-import {useState} from "react";
+import { Blocks, Moon, SunIcon } from "lucide-react";
+import { useState, useEffect } from "react";
 
 const MyHeader = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
+    const setIsDarkModeHandler = () => {
+        setIsDarkMode(!isDarkMode);
+    };
 
-    const [themeToggle , setThemeToggle] = useState("dark");
-
-    const handleSetThemeToggle=()=>{
-        setThemeToggle((themeToggle)=>{
-            if (themeToggle==="dark"){
-                setThemeToggle("light");
-            } else if(themeToggle==="light"){
-                setThemeToggle("dark");
-            }
-        });
-    }
+    // apply theme class on <body>
+    useEffect(() => {
+        if (isDarkMode) {
+            document.body.classList.add("dark");
+            document.body.classList.remove("light");
+        } else {
+            document.body.classList.add("light");
+            document.body.classList.remove("dark");
+        }
+    }, [isDarkMode]);
 
     return (
         <div className="header">
             <div>
-                <Blocks color="#F05C50" size="3rem"/>
+                <Blocks color="#F05C50" size="3rem" />
                 <h2>Extensions</h2>
             </div>
-            <button onClick={handleSetThemeToggle}>
-                {themeToggle==="dark" ? <SunIcon size="1.5rem" color="#fff"/> : <Moon size="1.5rem"/>}
+            <button onClick={setIsDarkModeHandler}>
+                {isDarkMode ? (
+                    <Moon size="1.5rem" />
+                ) : (
+                    <SunIcon size="1.5rem"/>
+                )}
             </button>
         </div>
-    )
-}
-export default MyHeader
+    );
+};
+
+export default MyHeader;
